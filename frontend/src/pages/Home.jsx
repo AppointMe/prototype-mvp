a8f16cfa54114908a2fccfe45333596af8ea541d
Mark Albrand <mark.albrand5@gmail.com>
2025-09-10 00:06:27 -0600

    Revert "Fix import paths to include .jsx extensions"
    This reverts commit 447b0d7590b42afc43c71c22cc351abf6a218335.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { pb } from "@/lib/pocketbase.js";
 
-import Upcoming from "@/components/Home/Upcoming.jsx";
-import Calendar from "@/components/Home/Calendar.jsx";
-import PastAppointments from "@/components/Home/PastAppointments.jsx";
+import Upcoming from "@/components/Home/Upcoming";
+import Calendar from "@/components/Home/Calendar";
+import PastAppointments from "@/components/Home/PastAppointments";
+import Navbar from "@/components/navbar/navbar.jsx";
 
 export default function Home() {
     const [appointments, setAppointments] = useState([]);
